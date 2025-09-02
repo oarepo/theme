a692b532e0f3ba1734a71b207db6757c7f93f1d0
Miroslav Bauer <mirekys@gmail.com>
2025-09-02 16:59:23 +0200

    add theme config

---
+++ oarepo_theme/webpack.py
@@ -0,0 +1,21 @@
+from invenio_assets.webpack import WebpackThemeBundle
+
+theme = WebpackThemeBundle(
+    __name__,
+    "assets",
+    default="oarepo",
+    themes={
+        "semantic-ui": {},
+        "oarepo": {
+            "entry": {},
+            "dependencies": {},
+            "devDependencies": {},
+            "aliases": {
+                "../../theme.config$": "less/theme.config",
+                "../../less/site": "less/site",
+                "../../less": "less",
+                "@less": "less",
+            },
+        }
+    },
+)
