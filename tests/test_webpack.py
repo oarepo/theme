c3b54bbf461f4c167d7ed9d5195b0838fd41122a
Miroslav Bauer <mirekys@gmail.com>
2025-09-04 09:28:54 +0200

    chore: code reformat

--- tests/test_webpack.py
+++ tests/test_webpack.py
@@ -1,14 +1,14 @@
-"""OARepo theme test suite for webpack integration.
-"""
+"""OARepo theme test suite for webpack integration."""
+
 from oarepo_runtime.cli.assets import enumerate_assets
 
 
 def test_webpack_themes(ui_app):
     with ui_app.app_context():
         aliases, asset_dirs, generated_paths = enumerate_assets()
-        assert '../../theme.config$' in aliases
-        assert '../../less/site' in aliases
-        assert '../../less' in aliases
-        assert '@less' in aliases
-        assert 'themes/oarepo' in aliases
+        assert "../../theme.config$" in aliases
+        assert "../../less/site" in aliases
+        assert "../../less" in aliases
+        assert "@less" in aliases
+        assert "themes/oarepo" in aliases
         assert generated_paths == []
