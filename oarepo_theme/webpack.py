abb6e872b9afc7c8a80eaea32b9bd1d235778069
Miroslav Bauer <mirekys@gmail.com>
2025-09-04 09:28:16 +0200

    test: setup tests for webpack

--- oarepo_theme/webpack.py
+++ oarepo_theme/webpack.py
@@ -1,3 +1,16 @@
+#
+# Copyright (c) 2025 CESNET z.s.p.o.
+#
+# This file is a part of oarepo-theme (see https://github.com/oarepo/theme).
+#
+# oarepo-theme is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+"""OARepo theme webpack integration module.
+
+This module provides webpack bundle project integration for OARepo SemanticUI theme.
+"""
+
 from invenio_assets.webpack import WebpackThemeBundle
 
 theme = WebpackThemeBundle(
