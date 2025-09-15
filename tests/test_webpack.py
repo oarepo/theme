3e6a298fcdb3b79ab606ad905e62046ed4f49ef4
Dusan Stojanovic <dusanstojanovic256@gmail.com>
2025-09-15 09:19:38 +0200

    fix: fixing issue with site overrides being overwritten by semantic ui defaults

--- tests/test_webpack.py
+++ tests/test_webpack.py
@@ -15,7 +15,7 @@ from oarepo_runtime.cli.assets import enumerate_assets
 
 def test_webpack_themes(ui_app):
     with ui_app.app_context():
-        aliases, asset_dirs, generated_paths = enumerate_assets()
+        aliases, _asset_dirs, generated_paths = enumerate_assets()
         assert "../../theme.config$" in aliases
         assert "../../less/site" in aliases
         assert "../../less" in aliases
