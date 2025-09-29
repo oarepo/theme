4d606e051e21964b3cc8aa8313bbf0356f2f4208
Miroslav Bauer <mirekys@gmail.com>
2025-09-29 15:20:27 +0200

    fix(tests): enumerate assets

--- tests/test_webpack.py
+++ tests/test_webpack.py
@@ -9,8 +9,30 @@
 """OARepo theme test suite for webpack integration."""
 
 from __future__ import annotations
+from importlib.metadata import entry_points
 
-from oarepo_runtime.cli.assets import enumerate_assets
+from flask import current_app
+from flask_webpackext import current_webpack
+
+
+def enumerate_assets():
+    asset_dirs = []
+    generated_paths = []
+    aliases = {}
+    themes = current_app.config["APP_THEME"] or ["semantic-ui"]
+    project = current_webpack.project
+    if hasattr(project, 'generated_paths'):
+        generated_paths += project.generated_paths
+
+    for ep in entry_points(group="invenio_assets.webpack"):
+        webpack = ep.load()
+        for wp_theme_name, wp_theme in webpack.themes.items():
+            if wp_theme_name in themes:
+                asset_dirs.append(wp_theme.path)
+                if hasattr(wp_theme, "generated_paths"):
+                    generated_paths += list(set(wp_theme.generated_paths) - set(generated_paths))
+                aliases.update(wp_theme.aliases)
+    return aliases, asset_dirs, generated_paths
 
 
 def test_webpack_themes(ui_app):
