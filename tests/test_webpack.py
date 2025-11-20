5d2cf8ec5b543efcd8b13eae555e690972cb99da
Miroslav Bauer <mirekys@gmail.com>
2025-11-20 10:35:05 +0100

    chore: rename package to oarepo-theme

--- tests/test_webpack.py
+++ tests/test_webpack.py
@@ -1,7 +1,7 @@
 #
 # Copyright (c) 2025 CESNET z.s.p.o.
 #
-# This file is a part of oarepo-theme (see https://github.com/oarepo/theme).
+# This file is a part of oarepo-theme (see https://github.com/oarepo/oarepo-theme).
 #
 # oarepo-theme is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
